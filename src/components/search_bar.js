b9410ac5b6735608091f411ab50bf626c65a494b
Kaylie Weable <kaylie@Kaylies-MacBook-Pro.local>
2017-06-10 13:33:40 -0700

    added event and state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
-  render() {
-    return <input />;
+  constructor(props) {
+    super(props);
+    //set state
+    this.state = { term: ''};
   }
+  render() {
+    //{} for vanilla elements
+    //event handler
+    return <input onChange={event => console.log(event.target.value)} />;
+  }
+//event
+//   onInputChange(event) {
+//     console.log(event.target.value);
+//   }
 }
 
 export default SearchBar;
