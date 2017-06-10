62b093bd94b62fecb1bf75fd605b9cd41fc9afb7
Kaylie Weable <kaylie@Kaylies-MacBook-Pro.local>
2017-06-10 10:42:12 -0700

    created search bar for videos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCbVgju_QyNamqdVp2Sov2gKyWykZoEnfw';
 //create a new component that produces some HTML
 //const is just an unmutable variable in ES6
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar/>
+    </div>
+  );
 }
 
 //put the component's generated HTML into the DOM
