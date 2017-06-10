c2bd4ebc279537648db80aec80a45d2181053425
Kaylie Weable <kaylie@Kaylies-MacBook-Pro.local>
2017-06-10 14:25:34 -0700

    added video detailcode and export

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,23 @@
 import React from 'react';
+//the viceo in {} declaes the variable video for us
+const VideoListItem = ({video}) => {
+  const imageUrl = video.snippet.thumbnails.default.url;
 
-const VideoListItem = (props) => {
-  return <li>Video</li>
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl}/>
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">
+          {video.snippet.title}
+          </div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCbVgju_QyNamqdVp2Sov2gKyWykZoEnfw';
 //create a new component that produces some HTML
@@ -24,6 +25,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar/>
+        <VideoDetail video={this.state.video[0]}/>
         <VideoList videos={this.state.videos}/>
       </div>
       );
